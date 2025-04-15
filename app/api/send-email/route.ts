832ea6329e2f5f97dca7b1e1e5a9ea5b090d0766
Nico Gomez <nicolas.gomez2101@alumnos.ubiobio.cl>
2025-04-15 13:55:28 -0400

    change .env

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -18,13 +18,18 @@ export async function POST(request: Request) {
             from: process.env.EMAIL_USER,
             to: 'nicogomezmorgado86@gmail.com',
             subject: `[CONTACTO] ${data.asunto}`,
-            html: `
-        <p><strong>Nombre:</strong> ${data.nombre}</p>
-        <p><strong>Email:</strong> ${data.email}</p>
-        <p><strong>Asunto:</strong> ${data.asunto}</p>
-        <p><strong>Mensaje:</strong></p>
-        <p>${data.mensaje}</p>
-        `
+            html: 
+                `
+                <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
+                    <h2 style="color: #4CAF50; text-align: center;">Nuevo Mensaje de Contacto</h2>
+                    <p><strong>Nombre:</strong> ${data.nombre}</p>
+                    <p><strong>Email:</strong> <a href="mailto:${data.email}" style="color: #1E90FF;">${data.email}</a></p>
+                    <p><strong>Mensaje:</strong></p>
+                    <div style="background-color: #f9f9f9; padding: 15px; border-radius: 5px; border: 1px solid #eee;">
+                        <p>${data.mensaje}</p>
+                    </div>
+                </div>
+                `,
         })
 
         return NextResponse.json({
